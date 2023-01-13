77f854b01952b877b7377b0545cdd52bf4007e0b
AyaShiau <63468278+adbshiau@users.noreply.github.com>
2023-01-12 17:21:06 -0800

    staff view, url and template created

--- inventory/views.py
+++ inventory/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, "inventory/index.html")
+
+def staff(request):
+    return render(request, "inventory/staff.html")
